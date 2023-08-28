d1c5a27f7a8cd80ce2c53bb54a7f68c632312c27
Ruairi Mullally <123960178+RuairiMullally@users.noreply.github.com>
2023-08-28 12:33:52 +0100

    Update main.py

--- Budget-App/main.py
+++ Budget-App/main.py
@@ -1,5 +1,5 @@
 class Category:
-  
+
   def __init__(self, category):
     self.category = category
     self.ledger = list()
@@ -9,12 +9,10 @@ class Category:
 
     for record in self.ledger:
       masterString += f"\n{record['description'][:23]:23}{record['amount']:7.2f}"
-       
+
     masterString += f"\nTotal: {str(self.get_balance())}"
     return masterString
 
-  
-    
   def get_balance(self):
     balance = 0
     for element in self.ledger:
@@ -25,9 +23,8 @@ class Category:
     count = self.get_balance()
     if amount > count:
       return False
-    else: return True
-
-  
+    else:
+      return True
 
   def deposit(self, amount, description=None):
 
@@ -45,24 +42,24 @@ class Category:
       dict = {"amount": amount, "description": description}
       self.ledger.append(dict)
       return True
-    else: return False
-      
+    else:
+      return False
 
   def transfer(self, amount, otherCat):
     if self.check_funds(amount):
-      description = f"Transfer to "+otherCat.category
+      description = f"Transfer to " + otherCat.category
       self.withdraw(amount, description)
-      description = "Transfer from "+self.category
+      description = "Transfer from " + self.category
       otherCat.deposit(amount, description)
       return True
-    else: return False
-
+    else:
+      return False
 
 
 def create_spend_chart(categories):
   masterString = "Percentage spent by category\n"
   values = list()
-  total =0
+  total = 0
   percentages = list()
   labels = list()
 
@@ -70,39 +67,39 @@ def create_spend_chart(categories):
     labels.append(category.category)
     spent = 0
     for i in category.ledger:
-      if i['amount'] <0:
+      if i['amount'] < 0:
         spent += i['amount']
-      values.append(spent)
+    values.append(spent)
 
   for value in values:
     total += value
 
   for j in values:
-    percentages.append(round((j/total)*100, -1))
-    
+    percentages.append((((j / total) * 100) // 10) * 10)
+
+
   k = 100
-  while k >=0 :
+  while k >= 0:
     masterString += f"{k:3}|"
     for elem in percentages:
       if elem >= k:
         masterString += ' o '
-      else: masterString += '   '
-    masterString += '\n'
-    k-=10
+      else:
+        masterString += '   '
+    masterString += ' \n'
+    k -= 10
   masterString += '    '
   for elem in percentages:
     masterString += '---'
   masterString += '-\n    '
 
-  
-  for p in range(len(max(labels , key = len))):
+  for p in range(len(max(labels, key=len))):
     for q in range(len(labels)):
-      try: 
+      try:
         masterString += f' {labels[q][p]} '
       except:
         masterString += f'   '
-    masterString += '\n    '
+    masterString += ' \n    '
 
-  print(masterString)
+  #print(masterString)
   return masterString
-      
